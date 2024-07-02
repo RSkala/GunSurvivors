f356d8c18cde9b6fdb963239e0e868da1159b7c5
RSkala <RichardSkala@gmail.com>
2024-07-02 12:37:41 -0700

    Added gamemode class and blueprint, set to defaullt game mode, set BP_Player as default player pawn in game mode bp

---
+++ Source/GunSurvivors/Public/GunSurvivorsGameMode.h
@@ -0,0 +1,17 @@
+// Copyright 2024 Richard Skala
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GunSurvivorsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNSURVIVORS_API AGunSurvivorsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
